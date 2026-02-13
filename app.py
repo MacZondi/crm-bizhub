74da10cd70fa41c9bc1c9248f928e08042d030e5
Uyanda <uyandasibiya@gmail.com>
2026-02-13 15:15:39 +0200

    frontend of code

--- app.py
+++ app.py
@@ -219,3 +219,6 @@ if __name__ == "__main__":
     print("Press Ctrl+C to stop")
     print()
     app.run(debug=True, host="127.0.0.1", port=5000)
+
+
+#Uyanda and Nhlanhla contributed to this code.
